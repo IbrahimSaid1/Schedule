6ac1de5d7b983f35ca51af1ba79555a5f83dd1f5
IbrahimSaid1 <squ118977@gmail.com>
2023-05-31 11:33:04 +0400

    Problem 1

---
+++ src/main/java/com/codeline/Scheduling/Controller/schedule.java
@@ -0,0 +1,15 @@
+package com.codeline.Scheduling.Controller;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class schedule {
+
+
+    @Scheduled(cron = "0 */5 * * * *")
+    public void scheuldeTask(){
+        System.out.print("Hello Word");
+    }
+}
