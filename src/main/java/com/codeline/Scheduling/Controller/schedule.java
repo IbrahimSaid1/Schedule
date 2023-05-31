5116a98e22350646cc47403b6719253590587349
IbrahimSaid1 <squ118977@gmail.com>
2023-05-31 11:38:59 +0400

    Problem 2

--- src/main/java/com/codeline/Scheduling/Controller/schedule.java
+++ src/main/java/com/codeline/Scheduling/Controller/schedule.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class schedule {
 
 
-    @Scheduled(cron = "0 */5 * * * *")
+    @Scheduled(cron = "0 0 * * * *")
     public void scheuldeTask(){
         System.out.print("Hello Word");
     }
