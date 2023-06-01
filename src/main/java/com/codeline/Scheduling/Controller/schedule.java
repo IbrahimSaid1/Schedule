c9b0a29bba1b2214b945a238e4f3af20078fef1a
IbrahimSaid1 <squ118977@gmail.com>
2023-06-01 16:29:37 +0400

    Problem 39

--- src/main/java/com/codeline/Scheduling/Controller/schedule.java
+++ src/main/java/com/codeline/Scheduling/Controller/schedule.java
@@ -11,6 +11,8 @@ public class schedule {
     @Scheduled(cron = "0 0 9-17 ? * *")
     @Scheduled(cron = "0 0/15 14-22 ? * 5,6")
     @Scheduled(cron = "0 0/10 8-15 ? * *")
+    @Scheduled(cron = "0 0 23 * * *")
+
 
     public void scheuldeTask(){
         System.out.print("Hello Word");
